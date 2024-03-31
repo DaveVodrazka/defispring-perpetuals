40c2cbec8fdd9faf925ac59456ccf1fb1b80470d
Chepelau <chepelau.a@gmail.com>
2024-03-31 19:48:17 +0200

    Add market identifier

--- src/0_carmine.py
+++ src/0_carmine.py
@@ -191,7 +191,7 @@ async def main():
         final[pool] = {
             "protocol": "Carmine",
             "date": date,
-            "market": "TODO",
+            "market": pool.upper(),
             "tokenSymbol": "TODO",
             "block_height": latest_block,
             "funding_rate": 0,
